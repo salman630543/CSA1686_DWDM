2df8ec5de89babdb5b3c77d51a5da70d556411db
salman630543 <112232446+salman630543@users.noreply.github.com>
2023-05-07 18:53:35 +0530

    Create MinMax_age(Diabetes).r

---
+++ MinMax_age(Diabetes).r
@@ -0,0 +1,8 @@
+diabetesX=read.csv("diabetes.csv")
+A=c(diabetesX$Age)
+View(diabetesX)
+Mean=mean(A)
+Minimum=min(diabetesX$Age)
+Maximum=max(diabetesX$Age)
+MinMax=(A-Minimum)/(Maximum-Minimum)
+print(MinMax)
